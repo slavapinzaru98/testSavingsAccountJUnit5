7d17a89c2b6cc531f2ceba520f5fd5ead67aa1e4
VPinzaru <Veaceslav.Pinzaru@endava.com>
2022-04-21 15:14:43 +0300

    second commit

---
+++ src/test/java/Input.java
@@ -0,0 +1,5 @@
+public class Input {
+    public static final double NEGATIVE_INPUT = -3000.00;
+    public static final double POSITIVE_INPUT = 3000.00;
+};
+
--- src/test/java/SavingsAccountTest.java
+++ src/test/java/SavingsAccountTest.java
@@ -1,29 +1,38 @@
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class SavingsAccountTest {
 
 
     @Test
-    void validDataInput(){
-        SavingsAccount savingsAccountValidInput = new SavingsAccount("11111111", 3000);
+    void getSavingsBalanceWithValidDataTest() {
+        SavingsAccount savingsAccountValidInput = new SavingsAccount("11111111", Input.POSITIVE_INPUT);
         SavingsAccount.annualInterestRate = 0.04;
 
         savingsAccountValidInput.calculateMonthlyInterest();
         double result = savingsAccountValidInput.getSavingsBalance();
-        assertEquals(3010.0,result);
+        assertEquals(3010.0, result);
     }
 
     @Test
-    void invalidDataInput(){
-        SavingsAccount savingsAccountInvalidInput = new SavingsAccount("11111111", -3000);
+    void getSavingsBalanceWithInvalidDataTest() {
+        SavingsAccount savingsAccountInvalidInput = new SavingsAccount("11111111", Input.NEGATIVE_INPUT);
         SavingsAccount.annualInterestRate = 0.04;
 
         savingsAccountInvalidInput.calculateMonthlyInterest();
         double result = savingsAccountInvalidInput.getSavingsBalance();
-        assertEquals(3010.0,result);
+        try {
+            if (result <= 0) {
+                throw new Exception("result must be positive");
+            } else {
+                assertEquals(3010.0, result);
+            }
+        } catch (Exception exception) {
+            assertEquals("result must be positive", exception.getMessage());
+        }
     }
 
-   }
+}
